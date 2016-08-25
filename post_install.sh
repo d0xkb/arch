ea74d0a1a7f98048e774e3a428822428a40f7f4c
Daniel Kubat <dakb@users.noreply.github.com>
2016-08-25 22:27:13 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,8 +4,8 @@
 pacman -Syu --noconfirm
 
 #SOFTWARE installation
-packages="chromium evince openssh transmission-gtk gvfs gvfs-smb bc mlocate bash-completion pkgstats arch-wiki-lite hdparm chrony"
-utils="iotop htop rsync wavemon wget"
+packages="chromium evince openssh transmission-gtk gvfs gvfs-smb mlocate bash-completion pkgstats arch-wiki-lite hdparm chrony"
+utils="iotop htop rsync wavemon wget bc"
 graphics="ristretto gimp"
 media="qt4 vlc python-pip"
 goodies="xfce4-cpugraph-plugin xfce4-netload-plugin xfce4-screenshooter xfce4-weather-plugin mousepad"
@@ -24,6 +24,7 @@ echo "options thinkpad_acpi fan_control=1" > /etc/modprobe.d/thinkpad_acpi.conf
 
 #PROFILE customizations
 echo "source /root/.bashrc" >> /etc/profile
+source /root/.bashrc
 
 cat > /root/.bashrc <<EOF
 #unlimited history length
@@ -33,22 +34,20 @@ HISTFILESIZE=
 alias ll='ls -la --color=auto'
 EOF
 
-source /root/.bashrc
-
 #SYSTEMD tuning
 echo "DefaultTimeoutStartSec=10s" >> /etc/systemd/system.conf
 echo "DefaultTimeoutStopSec=10s" >> /etc/systemd/system.conf
 
 #TIME/NTP config
-
 timedatectl set-ntp false
 timedatectl set-timezone Europe/Prague
 
 cat > /etc/chrony.conf <<EOF
-server ntp.nic.cz iburst
+server ntp.nic.cz prefer iburst
 server tik.cesnet.cz iburst
 server tak.cesnet.cz iburst
 driftfile /etc/chrony.drift
+deny all
 rtconutc
 rtcsync
 EOF
